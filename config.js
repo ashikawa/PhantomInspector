2d4751a50349f40e986aa862c1334b78edcb636f
a.shigeru <a.shigeru@gmail.com>
2013-12-06 19:28:10 +0900

    ファイル分割

--- config.js
+++ config.js
@@ -3,47 +3,14 @@
 
 (function () {
     'use strict';
-    var ejs        = require('ejs'),
-        _          = require('underscore'),
-        template   = require('fs').read('./tmpl/page.ejs'),
-        url        = require('url'),
-        analytics  = require('./analytics.js'),
+
+    var analytics = require('./lib/universal_analytics.js'),
+        metas     = require('./lib/metas.js'),
         setting;
 
     function analyticsRequest(page, networks) {
-
-        var records,
-            title,
-            metas;
-
-        records = _(networks).filter(function (element) {
-            return (element.url.indexOf('www.google-analytics.com/collect') !== -1);
-        });
-
-        title = page.evaluate(function () {
-            return $('title').text();
-        });
-
-        metas = page.evaluate(function () {
-            return $('meta[name],meta[property]').map(function (i, element) {
-                var $element = $(element);
-
-                return {
-                    'name':     $element.attr('name'),
-                    'property': $element.attr('property'),
-                    'content':  $element.attr('content'),
-                };
-            });
-        });
-
-        console.log(ejs.render(template, {
-            title: title,
-            metas: metas,
-            setting: this,
-            records: records,
-            url: url,
-            analytics: analytics
-        }));
+        console.log(metas.render(page, networks));
+        console.log(analytics.render(page, networks));
     }
 
     setting = [
@@ -52,7 +19,7 @@
             'url': 'http://www.example.com/',
             'callback': analyticsRequest,
             // 'userAgent': 'iPhone',
-            'capture': 'img/capture.png',
+            // 'capture': 'img/capture.png',
             // 'userName': 'xxxxx',
             // 'password': 'yyyyy',
         },
