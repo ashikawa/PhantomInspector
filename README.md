bbd9bdc63a81bddf07a21d54d914d85cdc361599
a.shigeru <a.shigeru@gmail.com>
2013-12-10 11:38:25 +0900

    マニュアルアップデート

--- README.md
+++ README.md
@@ -22,8 +22,13 @@ npm install
         {
             'name': 'トップ', // 見出し
             'url': 'http://example.com/', // アクセスするURL
-            'isrecord': myfunc, // 抽出条件
-            'capture': 'capture.png' // キャプチャ画像を保存
+            // 'onOpen': myfunc1, // ページ読み込み時に実行する関数
+            // 'timeout': myfunc2, // ページ読み込み後、一定時間後に実行する関数
+            // 'userAgent': 'iPhone', // userAgent 
+            // 'capture': 'img/capture.png',　// キャプチャ画像名
+            // 'viewportSize': {width: 400, height: 600}, // ウインドウサイズ
+            // 'userName': 'xxxxx', // Basic 認証ID
+            // 'password': 'yyyyy', // Basic 認証パスワード
         },
         {
             // ...
--- config.js
+++ config.js
@@ -6,32 +6,32 @@
 
     var base      = require('./lib/base.js'),
         analytics = require('./lib/universal_analytics.js'),
-        metas     = require('./lib/metas.js'),
+        // metas     = require('./lib/metas.js'),
         // catalyst  = require('./lib/catalyst.js'),
         setting;
 
     function analyticsRequest(page, networks, config) {
         console.log(base.render(page, networks, config));
-        console.log(metas.render(page, networks, config));
+        // console.log(metas.render(page, networks, config));
         console.log(analytics.render(page, networks, config));
         // console.log(catalyst.render(page, networks, config));
     }
 
     setting = [
         {
-            'name': 'トップ (PC)',
-            'url': 'http://www.example.com/',
-            'timeout': analyticsRequest,
-            // 'userAgent': 'iPhone',
-            // 'capture': 'img/capture.png',
-            // 'userName': 'xxxxx',
-            // 'password': 'yyyyy',
+            'name': 'トップ', // 見出し
+            'url': 'http://example.com/', // アクセスするURL
+            // 'onOpen': myfunc1, // ページ読み込み時に実行する関数
+            'timeout': analyticsRequest, // ページ読み込み後、一定時間後に実行する関数
+            // 'userAgent': 'iPhone', // userAgent 
+            // 'capture': 'img/capture.png',　// キャプチャ画像名
+            // 'viewportSize': {width: 400, height: 600}, // ウインドウサイズ
+            // 'userName': 'xxxxx', // Basic 認証ID
+            // 'password': 'yyyyy', // Basic 認証パスワード
         },
         // {
         //     'name': 'トップ (SP)',
         //     'url': 'http://example.com/',
-        //     'timeout': analyticsRequest,
-        //     'userAgent': 'iPhone'
         // }
     ];
 
